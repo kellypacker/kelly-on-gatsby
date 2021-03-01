3f70684573b88770acb5f6db0a7cb443bf8382cb
Kelly Packer <kelly@kellypacker.com>
2021-02-28 21:27:40 -0700

    Add art group loop

--- src/pages/artwork.js
+++ src/pages/artwork.js
@@ -1,41 +1,94 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import * as React from 'react';
-import Layout from '../components/layout';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-const ArtworkPage = () => {
-    const artGroups = useStaticQuery(graphql`
-        query {
-            allContentfulArtGroups {
-                edges {
-                    node {
-                        title
-                        artistStatement {
-                            raw
-                        }
-                        description {
-                            raw
-                        }
-                        image {
-                            file {
-                                url
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    `);
+const ArtworkContainerStyled = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, minmax(300px, 1fr));
+    grid-gap: 1rem 2rem;
+    border-bottom: 1px solid #dbd9d8;
+    :last-of-type {
+        border-bottom: none;
+    }
+`;
 
-    if (!artGroups.allContentfulArtGroups.edges) return null;
-    console.log(artGroups.allContentfulArtGroups.edges);
+const ImgContainer = styled.div`
+    border: 1px solid #dbd9d8;
+    padding: 3px;
+`;
+
+const ArtGroup = ({ artgroup }) => (
+    <ArtworkContainerStyled className="py-5">
+        <div>
+            <ImgContainer>
+                <Img
+                    className="artgroup-img"
+                    fluid={artgroup.image.fluid}
+                    alt={artgroup.title}
+                />
+            </ImgContainer>
+        </div>
+        <div className="">
+            <h2 className="text-salmon font-serif text-2xl font-bold pb-2">
+                {artgroup.title}
+            </h2>
+            {artgroup.description &&
+                documentToReactComponents(JSON.parse(artgroup.description.raw))}
+            {artgroup.artistStatement && (
+                <>
+                    <h3 className="uppercase text-lg pt-3 font-bold pb-2">
+                        Statement
+                    </h3>
+                    {documentToReactComponents(
+                        JSON.parse(artgroup.artistStatement.raw)
+                    )}
+                </>
+            )}
+        </div>
+    </ArtworkContainerStyled>
+);
+
+const ArtworkPage = ({ data }) => {
+    const artGroups = data?.allContentfulArtGroups?.nodes;
+    if (!artGroups) return null;
+    console.log(artGroups);
 
     return (
         <>
-            {artGroups.allContentfulArtGroups.edges.map((edge) => (
-                <h1 className="text-salmon font-serif">{edge.node.title}</h1>
+            <h1 className="text-3xl mt-4 uppercase">
+                Artwork <span className="italic lowercase">by</span> Series
+            </h1>
+
+            {artGroups.map((group) => (
+                <ArtGroup artgroup={group} key={group.id} />
             ))}
         </>
     );
 };
+
+export const query = graphql`
+    query {
+        allContentfulArtGroups(sort: { fields: publishedAt, order: DESC }) {
+            nodes {
+                description {
+                    raw
+                }
+                artistStatement {
+                    raw
+                }
+                title
+                id
+                image {
+                    fluid(maxWidth: 600, quality: 90) {
+                        ...GatsbyContentfulFluid
+                    }
+                }
+            }
+        }
+    }
+`;
 
 export default ArtworkPage;
