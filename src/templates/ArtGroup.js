042cedf84fa6263fa112939a501d0ac833ea51cf
Kelly Packer <kelly@kellypacker.com>
2021-03-01 22:50:26 -0700

    Artwork query

--- src/templates/ArtGroup.js
+++ src/templates/ArtGroup.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const ContainerStyled = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, minmax(300px, 1fr));
+    grid-gap: 1rem 2rem;
+    border-bottom: 1px solid #dbd9d8;
+    :last-of-type {
+        border-bottom: none;
+    }
+`;
 
 const ArtGroup = ({ data }) => {
     console.log(data);
-    const { artGroup } = data;
+    const { artGroup, artworks } = data;
     console.log(artGroup);
     return (
-        <h1 className="text-3xl mt-4 font-bold">
-            <span className="uppercase text-lg font-normal">Series:</span>{' '}
-            {artGroup.title}
-        </h1>
+        <>
+            <h1 className="text-3xl mt-4 mb-2 font-bold">
+                <span className="uppercase text-lg font-normal">Series:</span>{' '}
+                {artGroup.title}
+            </h1>
+            <ContainerStyled>
+                {artworks.nodes.map((artwork) => (
+                    <div key={artwork.id}>
+                        <Link
+                            to={`/artwork/series/${artGroup.slug}/${artwork.slug}`}
+                        >
+                            <Img
+                                fluid={artwork.image.fluid}
+                                alt={artwork.title}
+                            />
+                            <h3 className="text-center py-2">
+                                {artwork.title}
+                            </h3>
+                        </Link>
+                    </div>
+                ))}
+            </ContainerStyled>
+        </>
     );
 };
 
@@ -24,6 +55,21 @@ export const query = graphql`
     query($slug: String!) {
         artGroup: contentfulArtGroups(slug: { eq: $slug }) {
             title
+            slug
+        }
+        artworks: allContentfulArtwork(
+            filter: { artGroup: { slug: { eq: $slug } } }
+        ) {
+            nodes {
+                id
+                slug
+                title
+                image {
+                    fluid(maxWidth: 600, quality: 90) {
+                        ...GatsbyContentfulFluid
+                    }
+                }
+            }
         }
     }
 `;
