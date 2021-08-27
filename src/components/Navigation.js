6a6fef44cb5107bbb135a072502d4d5fb0d21d26
Kelly Packer <kelly@kellypacker.com>
2021-08-27 16:10:14 -0600

    Remove useResponsvie hooks
    Not working in build

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,9 +2,6 @@ import React, { useState } from 'react';
 import { PropTypes } from 'prop-types';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { useScreenMinMd } from '../hooks/useResponsive';
 
 const NavStyled = styled.nav`
     a[aria-current='page'] {
@@ -63,39 +60,37 @@ const StoreBgStyled = styled.span`
 
 const Navigation = ({}) => {
     const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
-    const isTablet = useScreenMinMd();
 
     const handleMobileNavToggle = () => {
         setIsMobileNavOpen(!isMobileNavOpen);
+        document.body.style.overflow = !isMobileNavOpen ? 'hidden' : 'auto';
     };
     return (
         <>
-            {!isTablet && (
-                <div className="relative main-nav-mobile">
-                    <Link className="main-nav-mobile__artwork" to="/artwork">
-                        <ArtworkBgStyled>Artwork</ArtworkBgStyled>
-                    </Link>
-                    {isMobileNavOpen && (
-                        <button
-                            type="button"
-                            onClick={handleMobileNavToggle}
-                            className="text-lg md:hidden"
-                        >
-                            Close
-                        </button>
-                    )}
-                    {!isMobileNavOpen && (
-                        <button
-                            type="button"
-                            onClick={handleMobileNavToggle}
-                            className="text-lg md:hidden"
-                        >
-                            More
-                        </button>
-                    )}
-                </div>
-            )}
-            <div className="main-header flex justify-between items-center border-b border-gray-md pb-2 pt-6">
+            <div className="block relative main-nav-mobile md:hidden">
+                <Link className="main-nav-mobile__artwork" to="/artwork">
+                    <ArtworkBgStyled>Artwork</ArtworkBgStyled>
+                </Link>
+                {isMobileNavOpen && (
+                    <button
+                        type="button"
+                        onClick={handleMobileNavToggle}
+                        className="text-lg md:hidden"
+                    >
+                        Close
+                    </button>
+                )}
+                {!isMobileNavOpen && (
+                    <button
+                        type="button"
+                        onClick={handleMobileNavToggle}
+                        className="text-lg md:hidden"
+                    >
+                        More
+                    </button>
+                )}
+            </div>
+            <div className="main-header flex justify-between items-center border-b border-gray-md pb-2 pt-4">
                 <Link
                     to="/"
                     className="text-3xl ml-1"
@@ -105,9 +100,9 @@ const Navigation = ({}) => {
                 </Link>
 
                 <div
-                    style={{
-                        display: isMobileNavOpen || isTablet ? 'block' : 'none',
-                    }}
+                    className={`${
+                        isMobileNavOpen ? 'block' : 'hidden'
+                    } md:block`}
                 >
                     <NavStyled>
                         <ul>
