3ba615a053eca82cf0b62fc5a83859d344514594
Kelly Packer <kelly@kellypacker.com>
2021-09-19 14:26:14 -0600

    Move css nav to comp

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { PropTypes } from 'prop-types';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
+import './css/nav.css';
 
 const NavStyled = styled.nav`
     a[aria-current='page'] {
